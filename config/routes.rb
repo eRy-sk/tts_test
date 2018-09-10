09bdb565c852b99a470ddb2410ac09ea04953b9d
Ryosuke Hiroe <ride.poke@gmail.com>
2018-09-10 15:20:35 +0900

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   root 'speak#index'
   post '/speak', to: 'speak#speak'
+  post '/bonus', to: 'speak#bonus'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
